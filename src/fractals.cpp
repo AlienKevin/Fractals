517e23cc498124d9e0ea8c307dcc92f078f33561
Kevin Li <kevinli020508@gmail.com>
2019-07-13 14:00:45 -0700

    Add doc to drawSierpinskiTriangle

--- src/fractals.cpp
+++ src/fractals.cpp
@@ -1,6 +1,15 @@
 #include "fractals.h"
 using namespace std;
 
+/**
+ * @brief Draw a Sierpinski Triangle at (x, y) with a side length and at a specific order
+ * @param window the graphic window
+ * @param x x coordinate to draw the triangle
+ * @param y y coordinate to draw the triangle
+ * @param size the size of the triangle or the length of its sides
+ * @param order the order of the triangle, function will draw 3 ^ order number of triangles.
+ *        0 order will draw nothing. Negative orders will throw an error.
+ */
 void drawSierpinskiTriangle(GWindow& window, double x, double y, double size, int order) {
     if (x < 0 || y < 0) {
         throw "x, y coordinates must be greater than or equal to 0!";
